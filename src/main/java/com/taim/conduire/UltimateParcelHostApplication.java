35c9251d3a0e8250ce1470d8bdc13277725936a6
Ulrich-Bertrand <ulrichbertrandmoffo@yahoo.fr>
2021-10-28 23:00:56 +0100

    base Project

---
+++ src/main/java/com/taim/conduire/UltimateParcelHostApplication.java
@@ -0,0 +1,13 @@
+package com.taim.conduire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateParcelHostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimateParcelHostApplication.class, args);
+    }
+
+}
